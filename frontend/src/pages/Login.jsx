6a315013bc30c81cdc5678a6fd749f9b4fb3df36
George-Ma2 <george.ma@upr.edu>
2024-12-15 21:34:09 -0400

    Added div to the form

--- frontend/src/pages/Login.jsx
+++ frontend/src/pages/Login.jsx
@@ -1,7 +1,13 @@
+import React from 'react';
+import { Link } from 'react-router-dom'
 import Form from '../components/Form'
 
 function Login() {
-    return <Form route='/api/token/' method='login' />
+    return (
+        <div>
+            <Form route='/api/token/' method='login' />
+        </div>
+    );
 }
 
 export default Login;
