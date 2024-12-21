e17ba52fe77beaf6bdd2be90d44f6defeb1d5ff7
George-Ma2 <george.ma@upr.edu>
2024-12-21 13:58:37 -0400

    Added urls path for inventory
    The paths that were added are for deleting, updating and listing the inventory

--- backend/api/urls.py
+++ backend/api/urls.py
@@ -5,4 +5,7 @@ urlpatterns = [
     # path('', views.home, name=''),
     path('notes/', views.NoteListCreate.as_view(), name='note-list'),
     path('notes/delete/<int:pk>/', views.NoteDelete.as_view(), name='delete-note'),
+    path('inventory/', views.InventoryListCreate.as_view(), name='inventory-list-create'),
+    path('inventory/<int:pk>/', views.InventoryRetrieveUpdateDestroy.as_view(), name='inventory-detail'),
+    path('inventory/update_quantity/<int:pk>/', views.UpdateProductQuantity.as_view(), name='update-product-quantity'),
 ]
