bc4cada828acbd4cd7133639ddd9870fddf837f7
Mariana Alvarez <mkag.alvarez@gmail.com>
2025-01-03 13:58:08 -0400

    Change settings password

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -104,7 +104,7 @@ DATABASES = {
         'ENGINE': 'django.db.backends.postgresql',
         'NAME': 'CARE_Web_App_DB',
         'USER': 'postgres',
-        'PASSWORD': 'postgres',
+        'PASSWORD': 'Pizzalover2003',
         'HOST':'localhost'
     }
 }
