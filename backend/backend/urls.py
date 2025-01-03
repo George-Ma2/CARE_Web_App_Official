e50a6a7cf95ae29301355adca5b4a21f675a640a
George-Ma2 <george.ma@upr.edu>
2025-01-03 16:19:05 -0400

    Eliminate spacing

--- backend/backend/urls.py
+++ backend/backend/urls.py
@@ -31,6 +31,5 @@ urlpatterns = [
     path('api/token/refresh/', TokenRefreshView.as_view(), name='refresh'),
     path('api-auth/', include('rest_framework.urls')),
     path('api/password_reset/',include('django_rest_passwordreset.urls', namespace='password_reset')), 
-    path('api/', include('api.urls')) # forward urls to urls from the api folder if it doesnt match any url from the backend url
-    
+    path('api/', include('api.urls')) # forward urls to urls from the api folder if it doesnt match any url from the backend urls.py
 ]
