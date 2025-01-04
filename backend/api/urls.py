be33986c1b0d5f6a5702479f405b089007a12059
George-Ma2 <george.ma@upr.edu>
2025-01-04 19:57:13 -0400

    added inventory update urls

--- backend/api/urls.py
+++ backend/api/urls.py
@@ -1,15 +1,15 @@
 from . import views
 from django.urls import path, include
 from rest_framework.routers import DefaultRouter
-from .views import UserViewSet, current_user_profile
 
 router = DefaultRouter()
-router.register(r'users', UserViewSet, basename='user')
+router.register(r'users', views.UserViewSet, basename='user')
 
 urlpatterns = [
-    path('profile/', current_user_profile, name='current_user_profile'),
+    path('profile/', views.current_user_profile, name='current_user_profile'),
     path('inventory/', views.InventoryListCreate.as_view(), name='inventory-list-create'),
     path('inventory/<int:pk>/', views.InventoryRetrieveUpdateDestroy.as_view(), name='inventory-detail'),
-    path('inventory/update_quantity/<int:pk>/', views.UpdateProductQuantity.as_view(), name='update-product-quantity')
+    path('inventory/update_quantity/<int:pk>/', views.UpdateProductQuantity.as_view(), name='update-product-quantity'),
+    path('inventory/update/<int:pk>/', views.update_product, name='update_product')
 ]
 
