2b3cfc81e888783e34f1740472798e40ff4b7f3e
George-Ma2 <george.ma@upr.edu>
2025-01-06 21:08:25 -0400

    Change care_package related_name

--- backend/api/models.py
+++ backend/api/models.py
@@ -127,9 +127,9 @@ class CarePackage(models.Model):
 
 
 class CarePackageItem(models.Model):
-    care_package = models.ForeignKey(CarePackage, related_name='items', on_delete=models.CASCADE)
+    care_package = models.ForeignKey(CarePackage, related_name='care_package_items', on_delete=models.CASCADE)
     product = models.ForeignKey(Inventory, on_delete=models.CASCADE)
-    quantity = models.PositiveIntegerField()
+    quantity = models.PositiveIntegerField(default=1)
 
     def __str__(self):
         return f"{self.product.name} x {self.quantity}"
