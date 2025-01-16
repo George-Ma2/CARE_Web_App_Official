70ea4cbf7ab8dc3d9062d706bb99a09ddda5f6ec
Mariana Alvarez <mkag.alvarez@gmail.com>
2025-01-16 11:46:22 -0400

    Migrations

--- backend/api/urls.py
+++ backend/api/urls.py
@@ -17,6 +17,7 @@ urlpatterns = [
     path('care-packages/oldest-package-date/', views.get_oldest_package_date),
     path('care-packages/same-create-date/', views.get_packages_with_same_create_date, name='packages-same-create-date'),
     path('order-history/create/', OrderHistoryCreateView.as_view(), name='order-history-create'),
+    path('care-packages/<int:pk>/delete/', views.CarePackageDeleteView.as_view(), name='care-package-delete'),
 
    # path('care-package', views.get_packages_with_same_issue_date, name='packages_same_issue_date'),
     # path('create-care-package/', views.CarePackageViewSet.as_view(), name='create-care-package'),
