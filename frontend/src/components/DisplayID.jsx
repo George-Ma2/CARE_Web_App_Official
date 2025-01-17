06a4d7f6bf204cc04ce2621de5924b6d21efca80
George-Ma2 <george.ma@upr.edu>
2025-01-17 18:27:43 -0400

    fix typo. Changed class to className

--- frontend/src/components/DisplayID.jsx
+++ frontend/src/components/DisplayID.jsx
@@ -162,24 +162,24 @@ function DisplayID() {
           </div>
         </main>
       </div>
-      <div class="footer">
-  <div class="footer-content">
+      <div className="footer">
+  <div className="footer-content">
   
-    <div class="footer-column">
-      <img src="/care.png" alt="Care logo" class="care-logo" />
-      <img src="/poli.png" alt="Polytechnic University of Puerto Rico logo" class="university-logo" />
+    <div className="footer-column">
+      <img src="/care.png" alt="Care logo" className="care-logo" />
+      <img src="/poli.png" alt="Polytechnic University of Puerto Rico logo" className="university-logo" />
       <p>© {new Date().getFullYear()} CARE</p>
       <p>Founded by Polytechnic University of Puerto Rico students</p>
     </div>
 
-    <div class="footer-column">
-      <p class="highlight">A non-profit student organization providing support to students in need.</p>
+    <div className="footer-column">
+      <p className="highlight">A non-profit student organization providing support to students in need.</p>
       <p>To learn more about our initiatives or ask any questions, please visit our social media pages!</p>
     </div>
  
-    <div class="footer-column">
+    <div className="footer-column">
       <h2>Follow Us</h2>
-      <ul class="social-links">
+      <ul className="social-links">
         
         <li><a href="https://www.instagram.com/care_pupr/" target="_blank">Instagram</a></li>
         <li><a href="https://www.linkedin.com/company/care-centro-de-apoyo-y-recursos-para-estudiantes/?viewAsMember=true" target="_blank">LinkedIn</a></li>
